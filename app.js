a5eb64f992701949d499871e576db19ab82fa1d7
engeeene <en9eeene@gmail.com>
2018-01-30 00:50:34 +0200

    create project with basic routes

---
+++ app.js
@@ -0,0 +1,46 @@
+
+// global modules and setup
+const path = require('path');
+const express = require('express');
+const bodyParser = require("body-parser");
+const app = express();
+const jsonParser = bodyParser.json();
+const hbs = require("hbs");
+app.set('views', __dirname + '/views');
+app.set("view engine", "hbs");
+
+// set partials
+hbs.registerPartials(__dirname + "/views/partials");
+
+// set static resources
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routing 
+app.get("/", function(request, response){
+	response.render('home.hbs',{
+		title: 'urban-wealth'
+	});
+});
+
+app.get("/district", function(request, response){
+	response.render('district.hbs',{
+		title: 'urban-wealth'
+	});
+});
+
+app.get("/event", function(request, response){
+	response.render('event.hbs',{
+		title: 'urban-wealth'
+	});
+});
+
+app.get("/stop", function(request, response){
+	console.log(`Server stop listening ${server.address().address}:${server.address().port}`);
+	console.log('Cause: The server has been shut down by the web request');
+	process.exit();
+	response.send('Server disabled');
+});
+
+var server = app.listen(3000, () => {
+	console.log(`Server start listening ${server.address().address}:${server.address().port}`);
+});
---
+++ public/js/core.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+	console.log('ready');
+	
+});
+
+
