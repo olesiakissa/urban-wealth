93e05b3e1bf08dd4cd11960a78cde0cb7ee9f8ae
yamorist <leiaorgana228@gmail.com>
2018-02-01 13:50:06 +0200

    - Add homepage - Add partials - Add script for button - Viewport fixes - Add styles

--- public/js/core.js
+++ public/js/core.js
@@ -1,6 +1,17 @@
-$(document).ready(() => {
+$(document).ready( function() {
 	console.log('ready');
-	
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('#top-btn').fadeIn();
+		} else {
+			$('#top-btn').fadeOut();
+		}
+	});
+
+	$('#top-btn').click(function(){
+		$('html, body').animate({scrollTop : 0}, 800);
+		return false;
+	});
+
 });
-
-
